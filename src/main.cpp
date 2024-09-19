a238acd4a2503e2343b85dfc5fa4651bd97fcf3a
Brokn_Apples <trentons708@gmail.com>
2024-09-19 13:04:25 -0500

    program setup #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <imgui/imgui.h>
+#include <vulkan/vulkan.hpp>
 
 int main(int argc, char* argv[]) {
-  std::cout << "Hello WOrld\n";
+    VkApplicationInfo app_info = {};
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pApplicationName = "AppEngine";
+    app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.pEngineName = "AppEngine";
+    app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.apiVersion = VK_API_VERSION_1_0;
 
-  std::cin.ignore(-1, '\n');
-  std::cin.get();
+    VkInstanceCreateInfo instance_info = {};
+    instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instance_info.pApplicationInfo = &app_info;
 
-  return 0; 
+    VkInstance vk_instance;
+
+    if (vkCreateInstance(&instance_info, nullptr, &vk_instance) == VK_SUCCESS) {
+        std::cout << "created instance\n";
+    } else {
+        std::cout << "Nah\n";
+    }
+
+    // Engine loop
+    while(false) {
+
+    }
+
+    vkDestroyInstance(vk_instance, NULL);
+
+    return 0;
 }
